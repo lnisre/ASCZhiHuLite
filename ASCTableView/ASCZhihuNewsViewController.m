bcafc80e9f34ce5460a7dd578c4097ad84b443bb
lnisre <lnisre@gmail.com>
2014-04-08 22:37:49 +0800

    1、采用UIWebView显示news内容   2、加入手势向右划跳至上页面

--- ASCTableView/ASCZhihuNewsViewController.m
+++ ASCTableView/ASCZhihuNewsViewController.m
@@ -10,10 +10,11 @@
 #import "ASCZhihuNewsManager.h"
 #import "ASCZhihuNews.h"
 
-@interface ASCZhihuNewsViewController ()
+@interface ASCZhihuNewsViewController () <UIWebViewDelegate>
 
 @property (retain, nonatomic) IBOutlet UIImageView *image;
 @property (retain, nonatomic) IBOutlet UITextView *context;
+@property (retain, nonatomic) IBOutlet UIWebView *webUI;
 @property (retain, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
@@ -24,25 +25,29 @@
 @synthesize context;
 @synthesize news;
 @synthesize scrollView;
+@synthesize webUI;
 
 -(void)setNews:(ASCZhihuNews *)anews
-{  
+{
     news = anews;
 
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:[NSURL URLWithString:anews.contextUrl]];
+    [self.webUI loadRequest:urlRequest];
+    
     [ASCZhihuNewsManager drawImageWithUrl:anews.imageUrl complete:^(UIImage *aimage) {
         [self.image setImage:aimage];
         self.scrollView.contentSize = CGSizeMake(320, self.image.bounds.size.height + self.context.bounds.size.height);
 
     }];
-    [ASCZhihuNewsManager fetchNewsWithUrl:anews.contextUrl complete:^(NSString *acontext) {
-        NSAttributedString *attributedString = [[NSAttributedString alloc] initWithData:[acontext dataUsingEncoding:NSUnicodeStringEncoding] options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType } documentAttributes:nil error:nil];
-        self.context.attributedText = attributedString;
-
-        self.context.frame = CGRectMake(self.context.frame.origin.x, self.context.frame.origin.y,
-                                         320, [attributedString size].height);
-        self.scrollView.contentSize = CGSizeMake(320, self.image.bounds.size.height + self.context.bounds.size.height);
-
-    }];
+//    [ASCZhihuNewsManager fetchNewsWithUrl:anews.contextUrl complete:^(NSString *acontext) {
+//        NSAttributedString *attributedString = [[NSAttributedString alloc] initWithData:[acontext dataUsingEncoding:NSUnicodeStringEncoding] options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType } documentAttributes:nil error:nil];
+//        self.context.attributedText = attributedString;
+//
+//        self.context.frame = CGRectMake(self.context.frame.origin.x, self.context.frame.origin.y,
+//                                         320, [attributedString size].height);
+//        self.scrollView.contentSize = CGSizeMake(320, self.image.bounds.size.height + self.context.bounds.size.height);
+//
+//    }];
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -55,14 +60,53 @@
         self.image.contentMode = UIViewContentModeScaleAspectFill;
         self.image.clipsToBounds = YES;
         [self.scrollView addSubview:self.image];
-        self.context = [[UITextView alloc] initWithFrame:CGRectMake(0, 170, 320, 400)];
-        [self.context setScrollEnabled:NO];
-        [self.scrollView addSubview:self.context];
+//        self.context = [[UITextView alloc] initWithFrame:CGRectMake(0, 170, 320, 400)];
+//        [self.context setScrollEnabled:NO];
+        
+        self.webUI = [[UIWebView alloc] initWithFrame:CGRectMake(0, 170, 320, 400)];
+        self.webUI.scrollView.scrollEnabled = NO;
+        self.webUI.delegate = self;
+        [self.view addSubview:self.webUI];
+        
+        [self.scrollView addSubview:self.webUI];
         
         [self.view addSubview:self.scrollView];
-
+        
+        UIPanGestureRecognizer *recognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleTranslation:)];
+        [self.view addGestureRecognizer:recognizer];
+        
     }
     return self;
+}
+
+- (void)handleTranslation:(UIPanGestureRecognizer*)recognizer
+{
+    if (recognizer.state == UIGestureRecognizerStateChanged || recognizer.state == UIGestureRecognizerStateEnded) {
+        CGPoint translate = [recognizer translationInView:self.view];
+        if (translate.x > 80) {
+            [self.navigationController popViewControllerAnimated:YES];
+        }
+    }
+    
+}
+
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    const CGFloat defaultWebViewHeight = 230.0;
+    //reset webview size
+    CGRect originalFrame = webView.frame;
+    webView.frame = CGRectMake(originalFrame.origin.x, originalFrame.origin.y, 320, defaultWebViewHeight);
+    CGSize actualSize = [webView sizeThatFits:CGSizeZero];
+    if (actualSize.height <= defaultWebViewHeight) {
+        actualSize.height = defaultWebViewHeight;
+    }
+    CGRect webViewFrame = webView.frame;
+    webViewFrame.size.height = actualSize.height;
+    webView.frame = webViewFrame;
+    
+    self.scrollView.contentSize = CGSizeMake(320, self.image.bounds.size.height + self.webUI.bounds.size.height);
+
 }
 
 - (void)viewDidLoad
